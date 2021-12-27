f1a48a726fac7fd953949db9f0780d5505f95d07
Greg <greg9381@gmail.com>
2021-12-26 19:37:40 -0500

    Add UT for calling the tune-creation endpoint twice

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,9 +1,15 @@
+import datetime
+
+from flask import Flask
+from flask.testing import FlaskClient
+
 from tuneshare.models.db import get_db
+from tuneshare.models.tune import list_tunes, Tune
 
 
 def test_create_tune_route(
-    app,
-    client,
+    app: Flask,
+    client: FlaskClient,
 ) -> None:
     test_tune_string = 'test-tune-string'
     response = client.post(
@@ -12,8 +18,49 @@ def test_create_tune_route(
     )
     with app.app_context():
         db = get_db()
-        result = db.execute('SELECT * FROM tune ORDER BY created_timestamp ASC')
-        items = list(result)
-        assert len(items) == 1, items
-        assert items[0]['encoded_tune'] == test_tune_string
-        assert items[0]['id'] == response.json['tune_id']
+        results = list_tunes(db)
+        assert len(results) == 1, results
+        validate_new_tune(
+            tune=results[0],
+            expected_id=response.json['tune_id'],
+            expected_encoded_tune=test_tune_string,
+        )
+
+
+def test_create_multiple_tunes(
+    app: Flask,
+    client: FlaskClient,
+) -> None:
+    test_tune_string = 'test-tune-string'
+    test_tune_string2 = 'test-tune-string2'
+    for i, tune_string in enumerate([test_tune_string, test_tune_string2], 1):
+        response = client.post(
+            '/',
+            json={'tune_string': tune_string}
+        )
+        with app.app_context():
+            db = get_db()
+            results = list_tunes(db)
+            # The first time, there will only be the first tune. The second
+            # time, both tunes will appear in the results, and we want to
+            # validate the second one (they are listed in ascending order of
+            # creation time).
+            assert len(results) == i, results
+            validate_new_tune(
+                tune=results[-1],
+                expected_id=response.json['tune_id'],
+                expected_encoded_tune=tune_string,
+            )
+
+
+def validate_new_tune(
+    tune: Tune,
+    expected_id: str,
+    expected_encoded_tune: str,
+) -> None:
+    assert tune.tune_id == expected_id
+    assert tune.encoded_tune == expected_encoded_tune
+    assert tune.created_timestamp == tune.last_accessed_timestamp
+    assert tune.access_count == 0
+    time_since_creation = datetime.datetime.now() - tune.created_timestamp
+    assert time_since_creation.total_seconds() < 1
--- tuneshare/models/tune.py
+++ tuneshare/models/tune.py
@@ -1,11 +1,39 @@
+import datetime
+import sqlite3
+from dataclasses import dataclass
 from sqlite3 import Connection
-
+from typing import List
 
 CREATE_SQL = '''
     INSERT INTO tune
     (encoded_tune)
     VALUES (?)
 '''
+
+SELECT_ALL_SQL = '''
+    SELECT *
+    FROM tune
+    ORDER BY created_timestamp ASC
+'''
+
+
+@dataclass
+class Tune:
+    tune_id: str
+    encoded_tune: str
+    created_timestamp: datetime.datetime
+    last_accessed_timestamp: datetime.datetime
+    access_count: int
+
+    @staticmethod
+    def from_db_row(row: sqlite3.Row) -> 'Tune':
+        return Tune(
+            tune_id=row['id'],
+            encoded_tune=row['encoded_tune'],
+            created_timestamp=row['created_timestamp'],
+            last_accessed_timestamp=row['last_accessed_timestamp'],
+            access_count=row['access_count'],
+        )
 
 
 def create_tune(db: Connection, tune_string: str) -> str:
@@ -14,3 +42,10 @@ def create_tune(db: Connection, tune_string: str) -> str:
     c.execute(CREATE_SQL, (tune_string,))
     db.commit()
     return c.lastrowid
+
+
+def list_tunes(db: Connection) -> List[Tune]:
+    """List all tunes in the database, in ascending order of creation time."""
+    c = db.cursor()
+    results = c.execute(SELECT_ALL_SQL)
+    return [Tune.from_db_row(row) for row in results]
