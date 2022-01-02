af066f38dde71ef7ad163bb9f074c6ec4f8110f1
Greg <greg9381@gmail.com>
2022-01-02 17:39:29 -0500

    Run Firestore emulator in GitHub Actions test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,14 +16,19 @@ def test_create_multiple_tunes(
     test_tune_string = 'test-tune-string'
     test_tune_string2 = 'test-tune-string2'
     for tune_string in (test_tune_string, test_tune_string2):
+        # Save tune
         post_response = client.post(
             '/api/tune', json={'tune_string': tune_string})
         tune_id = post_response.json['tune_id']
+
+        # Validate the tune that was just saved
+        created_at_client_time = datetime.now(tz=timezone.utc)
         get_response = client.get(f'/api/tune/{tune_id}')
         validate_new_tune(
             get_response,
             expected_id=tune_id,
             expected_encoded_tune=tune_string,
+            created_at_client_time=created_at_client_time,
         )
 
 
@@ -31,13 +36,14 @@ def validate_new_tune(
     get_response: Response,
     expected_id: str,
     expected_encoded_tune: str,
+    created_at_client_time: datetime,
 ) -> None:
     data = get_response.json
-    created_at_datetime = datetime.fromtimestamp(
+    created_at_server_time = datetime.fromtimestamp(
         data['created_at'], tz=timezone.utc)
     assert data['tune_id'] == expected_id
     assert data['tune_string'] == expected_encoded_tune
-    time_since_creation = datetime.now(tz=timezone.utc) - created_at_datetime
+    time_since_creation = created_at_client_time - created_at_server_time
     assert time_since_creation.total_seconds() < 0.5
 
 
